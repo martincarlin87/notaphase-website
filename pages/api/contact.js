eb3a2f107ea42a57d8b613a7e98b94de1e9bbe2c
Martin Carlin <martin@productguru.co.uk>
2022-11-15 01:44:56 +0000

    fix: use sendgrid api rather than smtp

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -2,6 +2,9 @@ import sendgrid from "@sendgrid/mail";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
+
+const SENDGRID_API = 'https://api.sendgrid.com/v3/mail/send'
+
 async function handleRequest(req, res) {
     // validate recaptcha
     await fetch("https://www.google.com/recaptcha/api/siteverify", {
@@ -39,33 +42,63 @@ async function handleRequest(req, res) {
 async function sendEmail(req) {
 
     try {
-        await sendgrid.send({
-            replyTo: req.body.email,
-            to: "hello@notaphase.band",
-            from: "hello@notaphase.band",
-            subject: '📩 New Website Contact Form Submission',
-            html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8">
-              
-                    <title>Website Contact Form Submission</title>
-                    <meta name="description" content="The HTML5 Herald">
-                    <meta name="author" content="SitePoint">
-                    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-                </head>
-                <body>
-                    <div class="container" style="margin-left: 20px;margin-right: 20px;">
-                        <h3>New website contact form submission from ${req.body.fullname}, their email is: ✉️ ${req.body.email}.</h3>
-                        <div style="font-size: 16px;">
-                            <p style="font-weight: bold;">Message:</p>
-                            <p>${req.body.message}</p>
-                        </div>
-                    </div>
-                </body>
-              </html>
-            `,
+
+        await fetch(SENDGRID_API, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.SENDGRID_API_KEY}`
+            },
+            body: JSON.stringify({
+                personalizations: [
+                    {
+                        to: [
+                            {
+                                email: "hello@notaphase.band",
+                                name: "Not A Phase"
+                            }
+                        ],
+                        subject: '📩 New Website Contact Form Submission'
+                    }
+                ],
+                from: {
+                    email: "hello@notaphase.band",
+                    name: "Not A Phase"
+                },
+                reply_to: {
+                    email: req.body.email,
+                    name: req.body.fullname
+                },
+                content: [
+                    {
+                        type: 'text/html',
+                        value:`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+                            <html lang="en">
+                            <head>
+                                <meta charset="utf-8">
+                          
+                                <title>Website Contact Form Submission</title>
+                                <meta name="description" content="The HTML5 Herald">
+                                <meta name="author" content="SitePoint">
+                                <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+                            </head>
+                            <body>
+                                <div class="container" style="margin-left: 20px;margin-right: 20px;">
+                                    <h3>New website contact form submission from ${req.body.fullname}, their email is: ✉️ ${req.body.email}.</h3>
+                                    <div style="font-size: 16px;">
+                                        <p style="font-weight: bold;">Message:</p>
+                                        <p>${req.body.message}</p>
+                                    </div>
+                                </div>
+                            </body>
+                          </html>
+                        `
+                    }
+                ]
+            })
         });
+
+
     } catch (error) {
         return error.statusCode || 500;
     }
